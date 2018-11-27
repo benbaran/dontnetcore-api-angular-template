8a722b949a5c15e672a8387a78d327eb966ebf53
Ben Baran <BBaran@Journeycare.org>
2018-11-27 10:37:22 -0600

    Added Serilog with SQL Server Sink

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-https://github.com/domaindrivendev/Swashbuckle.AspNetCore
 
 dotnet add package Swashbuckle.AspNetCore
 
@@ -15,3 +14,21 @@ app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
 });
+
+dotnet add package Serilog.AspNetCore
+dotnet add package Serilog.Settings.Configuration
+dotnet add package Serilog.Sinks.Console
+dotnet add package Serilog.Sinks.MssqlServer
+
+Configure CORS
+
+https://github.com/domaindrivendev/Swashbuckle.AspNetCore
+
+https://github.com/serilog/
+http://hamidmosalla.com/2018/02/15/asp-net-core-2-logging-with-serilog-and-microsoft-sql-server-sink/
+
+https://code-maze.com/net-core-web-development-part1/
+https://code-maze.com/net-core-web-development-part2/
+https://code-maze.com/net-core-web-development-part3/
+https://code-maze.com/net-core-web-development-part4/
+
--- web/Program.cs
+++ web/Program.cs
@@ -7,18 +7,50 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace web
 {
     public class Program
     {
+        // Get the appsettings.json for the current project
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                    .Build();
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            // Create the logger from the appsettings.json
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom
+                .Configuration(Configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host.");
+
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error starting host.");
+            }
+            finally
+            {
+                Log.Information("Shutting down host.");
+
+                Log.CloseAndFlush();
+            }
+
+            
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .UseSerilog()
+            .UseStartup<Startup>();
     }
 }
